858fea51ec66a888ac3b546a4163a0387f7b95e4
LoZzR <zakariaelhichem@gmail.com>
2022-07-26 12:04:36 +0100

    Use job parameters

--- hello-world/src/main/java/com/spring/batch/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/com/spring/batch/helloworld/HelloWorldApplication.java
@@ -6,13 +6,18 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -23,6 +28,9 @@ public class HelloWorldApplication {
 
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
+
+	@Autowired
+	private DataSource ds;
 
 	@Bean
 	public Step step() {
@@ -38,13 +46,42 @@ public class HelloWorldApplication {
 	}
 
 	@Bean
+	public Step step1() {
+		return this.stepBuilderFactory.get("step1")
+				.tasklet(helloWorldTasklet(null))
+				.build();
+	}
+	@Bean
+	public Tasklet helloWorldTasklet() {
+		return (contribution, chunkContext) -> {
+			String name = (String) chunkContext.getStepContext()
+					.getJobParameters()
+					.get("name");
+			System.out.println(String.format("Hello, %s!", name));
+			return RepeatStatus.FINISHED;
+		};
+	}
+
+	@StepScope
+	@Bean
+	public Tasklet helloWorldTasklet(
+			@Value("#{jobParameters['name']}") String name) {
+		return (contribution, chunkContext) -> {
+			System.out.println(String.format("Hello, %s!", name));
+			return RepeatStatus.FINISHED;
+		};
+	}
+
+	@Bean
 	public Job job() {
 		return this.jobBuilderFactory.get("job")
-				.start(step())
+				.start(step1())
 				.build();
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(HelloWorldApplication.class, args);
+		System.out.println(ctx);
+
 	}
 }
