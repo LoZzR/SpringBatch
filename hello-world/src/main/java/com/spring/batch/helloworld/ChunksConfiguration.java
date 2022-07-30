3c24e5737146d14b95966d640c94c7e35b568be8
Zakariae <zakariaelhichem@gmail.com>
2022-07-30 11:17:46 +0000

    Use CompletionPolicy to decide if a given chunk is complete

--- hello-world/src/main/java/com/spring/batch/helloworld/ChunksConfiguration.java
+++ hello-world/src/main/java/com/spring/batch/helloworld/ChunksConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.CompletionPolicy;
+import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,14 +25,33 @@ public class ChunksConfiguration {
     @Bean
     public Step chunkStep() {
         return this.stepBuilderFactory.get("chunkStep")
-                .<String, String>chunk(1000)
+                .<String, String>chunk(10)
                 .reader(itemReader())
                 .writer(itemWriter())
                 .build();
     }
+
+    @Bean
+    public Step chunkStepComposite() {
+        return this.stepBuilderFactory.get("chunkStepComposite")
+                .<String, String>chunk(completionPolicy())
+                .reader(itemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public CompletionPolicy completionPolicy() {
+        CompositeCompletionPolicy policy = new CompositeCompletionPolicy();
+        policy.setPolicies( new CompletionPolicy[] {
+                        new TimeoutTerminationPolicy(1),
+                        new SimpleCompletionPolicy(10)});
+        return policy;
+    }
+
     @Bean
     public ListItemReader<String> itemReader() {
-        List<String> items = new ArrayList<>(100000);
+        List<String> items = new ArrayList<>(100);
         for (int i = 0; i < 100; i++) {
             items.add(UUID.randomUUID().toString());
         }
